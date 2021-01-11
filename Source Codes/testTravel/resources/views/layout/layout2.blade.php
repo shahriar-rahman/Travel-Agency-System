56320ea3c3e642a25e18cb61490732dce16f5da9
shahriar-rahman <shahriar-rahman@users.noreply.github.com>
2021-01-11 13:28:24 +0600

    reupload

--- Source Codes/testTravel/resources/views/layout/layout2.blade.php
+++
@@ -1,6 +0,0 @@
-<body>
-    <div class = "container">
-        @include ('inc.messages')
-        @yield('layout2')
-    </div>
-</body>
--- Source Codes/testTravel/resources/views/pages/LoggedOut.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.layout1')
-
-@section('content')
-
-{!! Form::open(['action' => 'postsController@destroy', 'method' => 'GET', 'class'=> 'form-container']) !!}
-{{Form::submit ('Submit', ['class' => 'btn btn-primary'] ) }}
-@endsection
